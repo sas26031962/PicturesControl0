15b030690146e20151834901687bf7973e9c188d
sas26031962 <sasx26031962@gmail.com>
2025-04-11 17:22:05 +0300

    Внесение данных 2344

--- programm/moc_fmview.cpp
+++ programm/moc_fmview.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_fmView_t {
-    QByteArrayData data[6];
-    char stringdata0[53];
+    QByteArrayData data[8];
+    char stringdata0[88];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,11 +37,14 @@ QT_MOC_LITERAL(1, 7, 14), // "showExecStatus"
 QT_MOC_LITERAL(2, 22, 0), // ""
 QT_MOC_LITERAL(3, 23, 1), // "s"
 QT_MOC_LITERAL(4, 25, 8), // "execDraw"
-QT_MOC_LITERAL(5, 34, 18) // "execShowExecStatus"
+QT_MOC_LITERAL(5, 34, 18), // "execShowExecStatus"
+QT_MOC_LITERAL(6, 53, 32), // "execHorizontalSliderValueChanged"
+QT_MOC_LITERAL(7, 86, 1) // "x"
 
     },
     "fmView\0showExecStatus\0\0s\0execDraw\0"
-    "execShowExecStatus"
+    "execShowExecStatus\0execHorizontalSliderValueChanged\0"
+    "x"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +54,7 @@ static const uint qt_meta_data_fmView[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,11 +62,12 @@ static const uint qt_meta_data_fmView[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x06 /* Public */,
+       1,    1,   34,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       4,    1,   32,    2, 0x08 /* Private */,
-       5,    1,   35,    2, 0x08 /* Private */,
+       4,    1,   37,    2, 0x08 /* Private */,
+       5,    1,   40,    2, 0x08 /* Private */,
+       6,    1,   43,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QString,    3,
@@ -71,6 +75,7 @@ static const uint qt_meta_data_fmView[] = {
  // slots: parameters
     QMetaType::Void, QMetaType::QString,    3,
     QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::Int,    7,
 
        0        // eod
 };
@@ -84,6 +89,7 @@ void fmView::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         case 0: _t->showExecStatus((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 1: _t->execDraw((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 2: _t->execShowExecStatus((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 3: _t->execHorizontalSliderValueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -127,13 +133,13 @@ int fmView::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
--- programm/ui_fmview.h
+++ programm/ui_fmview.h
@@ -13,6 +13,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QDialog>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QSlider>
 
 QT_BEGIN_NAMESPACE
 
@@ -21,20 +22,29 @@ class Ui_fmView
 public:
     QLabel *labelMain;
     QLabel *label;
+    QSlider *horizontalSliderScale;
 
     void setupUi(QDialog *fmView)
     {
         if (fmView->objectName().isEmpty())
             fmView->setObjectName(QString::fromUtf8("fmView"));
-        fmView->resize(830, 850);
+        fmView->resize(830, 860);
         labelMain = new QLabel(fmView);
         labelMain->setObjectName(QString::fromUtf8("labelMain"));
         labelMain->setGeometry(QRect(5, 5, 821, 821));
         labelMain->setStyleSheet(QString::fromUtf8("background-color: rgb(239, 246, 255);"));
+        labelMain->setAlignment(Qt::AlignCenter);
         label = new QLabel(fmView);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(10, 830, 811, 16));
+        label->setGeometry(QRect(10, 830, 621, 22));
         label->setStyleSheet(QString::fromUtf8("background-color: rgb(248, 255, 250);"));
+        horizontalSliderScale = new QSlider(fmView);
+        horizontalSliderScale->setObjectName(QString::fromUtf8("horizontalSliderScale"));
+        horizontalSliderScale->setGeometry(QRect(640, 830, 181, 22));
+        horizontalSliderScale->setMinimum(410);
+        horizontalSliderScale->setMaximum(1640);
+        horizontalSliderScale->setValue(821);
+        horizontalSliderScale->setOrientation(Qt::Horizontal);
 
         retranslateUi(fmView);
 
